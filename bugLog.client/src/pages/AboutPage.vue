d1cf020794cda075604e05c05c1b88f56c24269e
Jonathan Wright <j.wright72127@gmail.com>
2021-08-13 15:23:18 -0600

    In class setup finished

--- bugLog.client/src/pages/AboutPage.vue
+++ bugLog.client/src/pages/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    <h1>I'm here to stay 😁</h1>
   </div>
 </template>
 
--- bugLog/server/db/DbContext.js
+++ bugLog/server/db/DbContext.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
-import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
+import { Bug } from '../models/bug'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Bugs = mongoose.model('Bug', Bug)
 }
 
 export const dbContext = new DbContext()
