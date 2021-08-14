8e9942e0d52d2791e4940c8ed821949b37ad9cef
Jonathan Wright <j.wright72127@gmail.com>
2021-08-14 12:03:53 -0600

    Notes Can now be created

--- bugLog/server/models/note.js
+++ bugLog/server/models/note.js
@@ -1,24 +1,23 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
-const ObjectId = Schema.Types.ObjectId
 
 export const Note = new Schema(
   {
     body: { type: String, required: true },
-    bug: { type: ObjectId, ref: 'Bug', required: true },
-    creatorId: { type: ObjectId, ref: 'Account', required: true }
+    bug: { type: Schema.Types.ObjectId, ref: 'Bug', required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 )
 Note.virtual('creator', {
-  localField: 'creatorId',
   ref: 'Account',
+  localField: 'creatorId',
   foreignField: '_id',
   justOne: true
 })
-Note.virtual('bug', {
-  localField: 'bug',
+Note.virtual('Bug', {
   ref: 'Bug',
-  foreignField: '_id',
+  localField: 'bug',
+  foreignField: 'id',
   justOne: true
 })
