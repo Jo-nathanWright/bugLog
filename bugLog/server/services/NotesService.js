d76f1b615159a8a08696194d5362d2dee0e61429
Jonathan Wright <j.wright72127@gmail.com>
2021-08-16 10:34:35 -0600

    Notes can now be deleted

--- bugLog/server/services/NotesService.js
+++ bugLog/server/services/NotesService.js
@@ -6,17 +6,25 @@ class NotesService {
     return await dbContext.Notes.find(query)
   }
 
+  async getById(id) {
+    const note = await dbContext.Notes.findById(id)
+    if (!note) {
+      throw new BadRequest('Invalid Id')
+    }
+    return note
+  }
+
   async create(body) {
     const note = await dbContext.Notes.create(body)
     return await dbContext.Notes.findById(note._id)
   }
 
   async remove(id, user) {
-    // const note = await this.getById(id)
-    // if (user.id === note.creatorId.toString()) {
-    //   await this.getById(id)
-    //   return await dbContext.Notes.findByIdAndDelete(id)
-    // }
+    const note = await this.getById(id)
+    if (user.id === note.creatorId.toString()) {
+      await this.getById(id)
+      return await dbContext.Notes.findByIdAndDelete(id)
+    }
   }
 }
 
